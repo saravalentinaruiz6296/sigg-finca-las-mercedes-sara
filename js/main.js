80b065db4d1fc7220789753bae0cec4acfc60868
Sara Valentina Avendaño Ruiz <sara.valentinaruiz6296@gmail.com>
2026-07-21 22:06:25 +0000

    perf(js): optimizar búsqueda de elementos del DOM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const enlaces = document.querySelectorAll('.modulo__enlace[aria-disabled="true"]');
+const enlaces = document.querySelector('.modulo__enlace[aria-disabled="true"]');
 
 enlaces.forEach(function (enlace) {
     enlace.addEventListener("click", function (evento) {
